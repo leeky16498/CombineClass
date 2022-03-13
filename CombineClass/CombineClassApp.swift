e0398cc825c9bc7d927e181c88bf69cf7e196cbe
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-13 08:24:34 +0000

    Initial Commit

---
+++ CombineClass/CombineClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineClassApp.swift
+//  CombineClass
+//
+//  Created by Kyungyun Lee on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
